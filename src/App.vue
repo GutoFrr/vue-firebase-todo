6a8a9b0181ad4b2a8990a524eb6223b1ccdc8940
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-10 12:10:40 -0300

    todo list initial setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AddTodo from './components/AddTodo.vue'
+</script>
+
+<template>
+  <div class="header">
+    <img src="./assets/vue.svg" class="logo vue-logo" />
+    <img src="./assets/firebase.svg" class="logo firebase-logo" />
+  </div>
+  <AddTodo />
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+}
+
+.logo {
+  width: 75px;
+  height: 75px;
+}
+</style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<template>
+  <div class="add-todo">
+    <input type="text" placeholder="Add todo" class="add-input" />
+    <button type="button" class="add-btn">Add</button>
+  </div>
+</template>
+
+<style scoped>
+.add-todo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+.add-input {
+  width: 300px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 2px solid var(--primary);
+  outline: none;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.25);
+  transition: all 0.3s;
+  color: #000;
+  font-family: 'Roboto';
+}
+
+.add-input:focus {
+  box-shadow: 0 4px 10px 2px rgba(0, 0, 0, 0.25);
+}
+
+.add-btn {
+  width: 75px;
+  padding: 10px;
+  border-radius: 5px;
+  border-color: transparent;
+  background-color: var(--primary);
+  color: #fff;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+  transition: all 0.3s;
+  font-family: 'Roboto';
+}
+
+.add-btn:hover {
+  filter: brightness(90%);
+  box-shadow: 0 4px 10px 2px rgba(0, 0, 0, 0.25);
+}
+</style>
